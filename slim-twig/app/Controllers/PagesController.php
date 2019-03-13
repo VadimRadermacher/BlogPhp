9c26195de55a0665c7fec127c6e05be2631c5652
grosjeanstephanie <grosjean.stephaniep@gmail.com>
2019-03-13 12:25:48 +0100

    allo

--- slim-twig/app/Controllers/PagesController.php
+++ slim-twig/app/Controllers/PagesController.php
@@ -22,14 +22,7 @@ class PagesController {
   public function register(RequestInterface $request, ResponseInterface $response){
     $this->container->view->render($response, 'pages/home.twig');
   }
-public function home(RequestInterface $request, ResponseInterface $response){
-  $result = $this->container->db->query("SELECT * FROM articles ORDER BY article_date DESC LIMIT 5")->fetchAll();
-//  var_dump($result[0]["article_date"]);
-  $this->container->view->render($response, 'pages/home.twig', ['result' => $result]);
-  }
-public function signup(RequestInterface $request, ResponseInterface $response){
-  $this->container->view->render($response, 'pages/signup.twig');
-  }
+
   
 public function login(RequestInterface $request, ResponseInterface $response, array $args){
   $user_name = 'nabil';
