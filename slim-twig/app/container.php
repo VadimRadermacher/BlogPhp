6ef872509b1f7cdcbf06ad3969d7f8d873e7f37a
Monticelli Romain <rmm.monticelli@gmail.com>
2019-03-15 07:42:38 +0100

    sketch dashboard + script db + configuring permissions

--- slim-twig/app/container.php
+++ slim-twig/app/container.php
@@ -13,32 +13,13 @@ $container = $app->getContainer();
     $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
     $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
     $view->getEnvironment()->addGlobal('session',$_SESSION);
-    //     $view->offsetSet('session', $_SESSION);
-     return $view;
+    return $view;
  };
-// view renderer
-// $container['renderer'] = function ($c) {
-//     $settings = $c->get('settings')['renderer'];
-//     return new Slim\Views\PhpRenderer($settings['template_path']);
-// };
 
-// monolog
-// $container['logger'] = function ($c) {
-//     $settings = $c->get('settings')['logger'];
-//     $logger = new Monolog\Logger($settings['name']);
-//     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-//     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-//     return $logger;
-// };
+
 
 $container['db'] = function($c) {
     
-    // $settings = $c->get('settings')['db'];
-    // $connstring = $settings['dbType'] . ':';
-    // unset($settings['dbType']);
-    // foreach ($settings as $key => $value) {
-    //     $connstring .= "$key=$value;";
-    // }
     $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=becode;user=becode;password=becode");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
